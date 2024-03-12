68527fc6bc9761b91ecf48078a97a124b6afefe3
GitButler <gitbutler@gitbutler.com>
2024-03-12 23:35:44 +0800

    GitButler Integration Commit
    This is an integration commit for the virtual branches that GitButler is tracking.
    
    Due to GitButler managing multiple virtual branches, you cannot switch back and
    forth between git branches and virtual branches easily. 
    
    If you switch to another branch, GitButler will need to be reinitialized.
    If you commit on this branch, GitButler will throw it away.
    
    Here are the branches that are currently applied:
     - Virtual branch (refs/gitbutler/Virtual-branch)
       - package.json
       - commitlint.config.js
       - pnpm-lock.yaml
       - commitlint.config.cjs
       - .gitignore
       - src/env/.env
       - .vscode/extensions.json
       - .vscode/settings.json
       - .vscode/vue3.code-snippets
       - src/pages/index/index.vue
       - src/pages.json
       - pages.config.ts
       - tsconfig.json
       - src/env/.env.test
       - src/env/.env.production
       - src/env/.env.development
       - vite.config.ts
       - src/env.d.ts
       - src/api/common.ts
       - src/utils/http.ts
       - src/typings/index.ts
       - src/store/index.ts
       - src/store/user/user.ts
       - src/store/count/useCountStore.ts
       - src/layouts/default.vue
       - uno.config.ts
       - src/manifest.json
       - uni-pages.d.ts
       - src/auto-import.d.ts
       - src/layouts/home.vue
       - src/main.ts
     - 特性: 🚀 小程序框架配置项优化
     (refs/gitbutler/-)
       branch head: 54bf2b0dfa258753297767235cc69207504f1fa4
    
    Your previous branch was: refs/heads/main
    
    The sha for that commit was: 563403cb54e2afcebc4416bd1e12aebb4aa93a55
    
    For more information about what we're doing here, check out our docs:
    https://docs.gitbutler.com/features/virtual-branches/integration-branch

--- src/env.d.ts
+++ src/env.d.ts
@@ -6,3 +6,15 @@ declare module '*.vue' {
   const component: DefineComponent<{}, {}, any>
   export default component
 }
+// 下面都是新增的
+interface ImportMetaEnv {
+  readonly VITE_APP_TITLE: string
+  readonly VITE_SERVER_PORT: string
+  readonly VITE_SERVER_BASEURL: string
+  readonly VITE_DELETE_CONSOLE: string
+  // 更多环境变量...
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
---
+++ src/layouts/default.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="default-layout">
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {
+  setup() {
+    return {}
+  },
+}
+</script>
+
+<style lang="scss" scoped></style>

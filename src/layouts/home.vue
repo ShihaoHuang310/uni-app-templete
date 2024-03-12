35c80fb28554b9213f5a992965dc14bea7fbcec2
GitButler <gitbutler@gitbutler.com>
2024-03-12 23:37:20 +0800

    GitButler Integration Commit
    This is an integration commit for the virtual branches that GitButler is tracking.
    
    Due to GitButler managing multiple virtual branches, you cannot switch back and
    forth between git branches and virtual branches easily. 
    
    If you switch to another branch, GitButler will need to be reinitialized.
    If you commit on this branch, GitButler will throw it away.
    
    Here are the branches that are currently applied:
     - Virtual branch (refs/gitbutler/Virtual-branch)
       - package.json
       - commitlint.config.js
       - pnpm-lock.yaml
       - commitlint.config.cjs
       - .gitignore
       - src/env/.env
       - .vscode/extensions.json
       - .vscode/settings.json
       - .vscode/vue3.code-snippets
       - src/pages/index/index.vue
       - src/pages.json
       - pages.config.ts
       - tsconfig.json
       - src/env/.env.test
       - src/env/.env.production
       - src/env/.env.development
       - vite.config.ts
       - src/env.d.ts
       - src/api/common.ts
       - src/utils/http.ts
       - src/typings/index.ts
       - src/store/index.ts
       - src/store/user/user.ts
       - src/store/count/useCountStore.ts
       - src/layouts/default.vue
       - uno.config.ts
       - src/manifest.json
       - uni-pages.d.ts
       - src/auto-import.d.ts
       - src/layouts/home.vue
       - src/main.ts
     - 特性: 🚀 小程序框架配置项优化
     (refs/gitbutler/-)
       branch head: 54bf2b0dfa258753297767235cc69207504f1fa4
     - Merge branch 'gitbutler/integration' of github.com:ShihaoHuang310/uni-app-templete into gitbutler/integration
     1 (refs/gitbutler/Merge-branch-gitbutler/integration-of-github.com-ShihaoHuang310/uni-app-templete-into-gitbutler/integration-1)
       branch head: bf9cdd64a555d5e80a024d5c8d9dbd206374eebf
     - Merge branch 'gitbutler/integration' of github.com:ShihaoHuang310/uni-app-templete into gitbutler/integration
     2 (refs/gitbutler/Merge-branch-gitbutler/integration-of-github.com-ShihaoHuang310/uni-app-templete-into-gitbutler/integration-2)
       branch head: bbde65285482ae3dabd5c3db8ee2b6dfc3573a56
     - Merge branch 'gitbutler/integration' of github.com:ShihaoHuang310/uni-app-templete into gitbutler/integration
     (refs/gitbutler/Merge-branch-gitbutler/integration-of-github.com-ShihaoHuang310/uni-app-templete-into-gitbutler/integration-)
       branch head: 69ad14b8fff93da527a64ba3f2537b3ee9225d95
    
    Your previous branch was: refs/heads/main
    
    The sha for that commit was: 563403cb54e2afcebc4416bd1e12aebb4aa93a55
    
    For more information about what we're doing here, check out our docs:
    https://docs.gitbutler.com/features/virtual-branches/integration-branch

---
+++ src/layouts/home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home-layout">
+    <slot />
+    2333
+  </div>
+</template>
+
+<script>
+export default {
+  setup() {
+    return {}
+  },
+}
+</script>
+
+<style lang="scss" scoped></style>
